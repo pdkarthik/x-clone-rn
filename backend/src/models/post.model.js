41547515f04dddfd3f6ace0eb6ad437dea23f3a5
Karthik <dkarthik874@gmail.com>
2025-08-01 08:19:57 +0530

    post.model modified

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -2,10 +2,31 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    content: { type: String, maxLength: 280 },
-    image: { type: String, default: "" },
-    likes: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      maxLength: 280,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
   { timestamps: true }
 );
